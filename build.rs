ebdbb6becc2005ae48daf47a642b74ddda06399e
Christopher Tee <christopherfytee@gmail.com>
2025-02-11 03:06:46 -0500

    Revert "Initial build for ONNX imports"
    This reverts commit cb388e50b95b69e0ac6e67f7a63537ec6b5dcc2d.

--- build.rs
+++
@@ -1,8 +0,0 @@
-use burn_import::onnx::ModelGen;
-
-fn main() {
-    ModelGen::new()
-        .input("../../rust-ml/onnx-optimum/distilbert_base_uncased_squad_onnx/model.onnx")
-        .out_dir("model/")
-        .run_from_script();
-}
